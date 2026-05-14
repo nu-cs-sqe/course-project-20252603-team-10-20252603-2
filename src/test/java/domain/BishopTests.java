2e8d8723c9eccf6f5ae60f07eecf09c1ef959e7c
samrine <samreenisme@gmail.com>
2026-05-14 15:28:53 -0500

    isValidMove_Bishop_OneDiagonalDownAndRight_returnTrue

--- src/test/java/domain/BishopTests.java
+++ src/test/java/domain/BishopTests.java
@@ -65,4 +65,18 @@ public class BishopTests {
 
         assertTrue(result);
     }
+
+    @Test
+    public void isValidMove_Bishop_OneDiagonalDownAndRight_returnTrue() {
+        Piece bishop = new Bishop(PieceColor.WHITE);
+
+        Location start = new Location(7, 0);
+        Location chosen = new Location(6, 1);
+
+        Board board = new Board(false);
+
+        boolean result = bishop.isValidMove(start, chosen, board);
+
+        assertTrue(result);
+    }
 }
