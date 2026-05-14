17929795f2fc5e1ac26936b3fe98f148fb5bb8ac
samrine <samreenisme@gmail.com>
2026-05-14 15:37:12 -0500

    isValidMove_Bishop_HorizontalMove_returnFalse() passes

--- src/test/java/domain/BishopTests.java
+++ src/test/java/domain/BishopTests.java
@@ -149,4 +149,18 @@ public class BishopTests {
 
         assertTrue(result);
     }
+
+    @Test
+    public void isValidMove_Bishop_HorizontalMove_returnFalse() {
+        Piece bishop = new Bishop(PieceColor.WHITE);
+
+        Location start = new Location(0, 0);
+        Location chosen = new Location(0, 3);
+
+        Board board = new Board(false);
+
+        boolean result = bishop.isValidMove(start, chosen, board);
+
+        assertFalse(result);
+    }
 }
