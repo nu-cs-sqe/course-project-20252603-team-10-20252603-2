2fb8a5890125dc326c918ee3312717b3bf367867
samrine <samreenisme@gmail.com>
2026-05-14 15:33:58 -0500

    isValidMove_Bishop_SevenDiagonalUpAndLeft_returnTrue() passes

--- src/test/java/domain/BishopTests.java
+++ src/test/java/domain/BishopTests.java
@@ -107,4 +107,18 @@ public class BishopTests {
 
         assertTrue(result);
     }
+
+    @Test
+    public void isValidMove_Bishop_SevenDiagonalUpAndLeft_returnTrue() {
+        Piece bishop = new Bishop(PieceColor.WHITE);
+
+        Location start = new Location(0, 7);
+        Location chosen = new Location(7, 0);
+
+        Board board = new Board(false);
+
+        boolean result = bishop.isValidMove(start, chosen, board);
+
+        assertTrue(result);
+    }
 }
