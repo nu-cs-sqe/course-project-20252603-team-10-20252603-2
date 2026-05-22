9346582e1eec8cd90a6c8c8bec246a52961521ba
crose-prw <crose.prw@gmail.com>
2026-05-22 00:12:02 -0500

    passing test isValidMove_Queen_xMovPosFriendlyBlock_returnFalse()

--- src/main/java/domain/piece/Queen.java
+++ src/main/java/domain/piece/Queen.java
@@ -16,14 +16,34 @@ public class Queen extends Piece {
     @Override
     public boolean isValidMove(Location start, Location end, Board board) {
 
+//        Location blocker = new Location(3, 0);
+//        if (board.isPieceHere(blocker) == true) return false;
+
         boolean xMovement = (start.getY() == end.getY()) && (start.getX() != end.getX());
         boolean yMovement = (start.getY() != end.getY()) && (start.getX() == end.getX());
+
         int diffX = Math.abs(start.getX() - end.getX());
         int diffY = Math.abs(start.getY() - end.getY());
+        int dirX = (Math.abs(start.getX() - end.getX()) == (start.getX() - end.getX())) ? -1 : 1;
+
         boolean diagonalMovement = (diffX == diffY);
 
+        if (diagonalMovement) return true;
 
-        if (xMovement || yMovement || diagonalMovement) return true;
+        if (yMovement) return true;
+
+        if (xMovement) {
+
+            for (int i = 1; i < diffX - 1; i++) {
+                Location locationCheck = new Location(start.getX() + (i * dirX), start.getY());
+
+                if (board.isPieceHere(locationCheck) == true) return false;
+
+            }
+            return true;
+        }
+
+
 
         //  TODO: complete method
         return false;
--- src/test/java/domain/QueenTests.java
+++ src/test/java/domain/QueenTests.java
@@ -1,10 +1,12 @@
 package domain;
 
+import domain.piece.Pawn;
 import domain.piece.Queen;
 import domain.piece.Piece;
 import domain.piece.PieceColor;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QueenTests {
@@ -106,5 +108,23 @@ public class QueenTests {
 
         assertTrue(result);
     }
+
+    @Test
+    public void isValidMove_Queen_xMovPosFriendlyBlock_returnFalse() {
+        Piece queen = new Queen(PieceColor.BLACK);
+        Piece blocker = new Pawn(PieceColor.BLACK);
+
+        Location start = new Location(0, 0);
+        Location chosen = new Location(7, 0);
+        Location blockerLocation = new Location(3, 0);
+
+        Board board = new Board(false);
+        board.setPiece(blockerLocation, blocker);
+
+        boolean result = queen.isValidMove(start, chosen, board);
+
+        assertFalse(result);
+    }
+
 
 }
