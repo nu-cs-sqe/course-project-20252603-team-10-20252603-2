214da230031eaae62e3b743e6e6cbeadaebc2197
crose-prw <crose.prw@gmail.com>
2026-06-11 20:39:34 -0500

    passing test hasValidMoves_Bishop_OnlyDownRightOpen_ReturnsTrue()

--- src/test/java/domain/piece/BishopTests.java
+++ src/test/java/domain/piece/BishopTests.java
@@ -531,9 +531,24 @@ public class BishopTests {
         Board board = new Board(false);
         board.setPiece(bishopPos, bishop);
 
-        board.setPiece(new Location(3, 3), new Pawn(Color.WHITE)); // up-left blocked
-        board.setPiece(new Location(3, 5), new Pawn(Color.WHITE)); // up-right blocked
-        board.setPiece(new Location(5, 5), new Pawn(Color.WHITE)); // down-right blocked
+        board.setPiece(new Location(3, 3), new Pawn(Color.WHITE));
+        board.setPiece(new Location(3, 5), new Pawn(Color.WHITE));
+        board.setPiece(new Location(5, 5), new Pawn(Color.WHITE));
+
+        assertTrue(bishop.hasValidMoves(bishopPos, board));
+    }
+
+    @Test
+    public void hasValidMoves_Bishop_OnlyDownRightOpen_ReturnsTrue() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Location bishopPos = new Location(4, 4);
+
+        Board board = new Board(false);
+        board.setPiece(bishopPos, bishop);
+
+        board.setPiece(new Location(3, 3), new Pawn(Color.WHITE));
+        board.setPiece(new Location(3, 5), new Pawn(Color.WHITE));
+        board.setPiece(new Location(5, 3), new Pawn(Color.WHITE));
 
         assertTrue(bishop.hasValidMoves(bishopPos, board));
     }
